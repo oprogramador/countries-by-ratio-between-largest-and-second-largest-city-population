a08759e963ac231435fd64763bd842c0769df178
oprogramador <uomodislesia@gmail.com>
2018-05-18 03:34:24 +0200

    script

---
+++ index.js
@@ -0,0 +1,28 @@
+const allTheCities = require('all-the-cities');
+const _ = require('lodash');
+const jsonToMarkdown = require('json-to-markdown');
+const countriesByISO = require('i18n-iso-countries');
+
+const englishNames = countriesByISO.getNames('en');
+const citiesByCountry = _.groupBy(allTheCities, 'country');
+const countries = Object.keys(citiesByCountry);
+const largestCities = _.mapValues(citiesByCountry, cities => _.maxBy(cities, 'population'));
+const secondLargestCities = _.mapValues(
+  citiesByCountry,
+  (cities, country) => _.maxBy(cities, city => largestCities[country].name !== city.name && city.population),
+);
+
+const result = countries.map(country => ({
+  country: englishNames[country],
+  'largest city name': largestCities[country].name,
+  'largest city population': largestCities[country].population,
+  'second largest city name': secondLargestCities[country].name,
+  'second largest city population': secondLargestCities[country].population,
+  'ratio': largestCities[country].population / secondLargestCities[country].population,
+}));
+
+const filteredResult = result.filter(item => item.ratio !== Infinity && item.ratio !== 1);
+
+const orderedResult = _.orderBy(filteredResult, item => -item.ratio);
+
+console.log(jsonToMarkdown(orderedResult, Object.keys(orderedResult[0])));
